62671d11c786841c3ca551821d02d6fb3f69e4fb
Maksim Zaharenkov <m.zakharenkov@list.ru>
2025-11-19 21:47:44 +0300

    Add Process and Generate the Profile

--- lecture_2/main.py
+++ lecture_2/main.py
@@ -4,12 +4,16 @@ def main():
     birth_year = int(birth_year_str)
     current_age = 2025 - birth_year
     hobbies: list[str] = []
+
     while True:
         next_input = input("Enter a favorite hobby or type 'stop' to finish: ")
         if next_input == "stop":
-            pass
+            break
         else:
             hobbies.append(next_input)
+            
+    life_stage = generate_profile(current_age)
+    user_profile = {"name": user_name, "age": current_age, "stage": life_stage, "hobbies": hobbies}
 
 
 def generate_profile(age: int) -> str:
