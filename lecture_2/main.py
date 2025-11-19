7fad099b19b3801014561ce2c45672c90937428f
Maksim Zaharenkov <m.zakharenkov@list.ru>
2025-11-19 21:32:16 +0300

    Add main function and base logic

--- lecture_2/main.py
+++ lecture_2/main.py
@@ -1,3 +1,17 @@
+def main():
+    user_name = input("Enter your full name: ")
+    birth_year_str = input("Enter your birth year: ")
+    birth_year = int(birth_year_str)
+    current_age = 2025 - birth_year
+    hobbies: list[str] = []
+    while True:
+        next_input = input("Enter a favorite hobby or type 'stop' to finish: ")
+        if next_input == "stop":
+            pass
+        else:
+            hobbies.append(next_input)
+
+
 def generate_profile(age: int) -> str:
     if 0 <= age <= 12:
         return "Child"
