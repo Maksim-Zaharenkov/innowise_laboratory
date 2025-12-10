cc4649dd8eae36fa6029b047a38da2915a3d581f
Maksim Zaharenkov <m.zakharenkov@list.ru>
2025-12-11 00:34:49 +0300

    Add all endpoints to project, add pagination

--- lecture_5/book_api/database.py
+++ lecture_5/book_api/database.py
@@ -1,12 +1,16 @@
+"""It is database connection module"""
+
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+file_path = os.path.join(os.path.dirname(os.getcwd()), "book_collection.db")
 
-SQL_DATABASE_URL = 'sqlite:///./lecture_5/book_collection.db'
+SQL_DATABASE_URL = 'sqlite:///' + file_path
 
 engine = create_engine(SQL_DATABASE_URL, connect_args={'check_same_thread': False})
 
-session_local = sessionmaker(autoflush=False, bind=engine)
+session_local = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
 Base = declarative_base()
--- lecture_5/book_api/models.py
+++ lecture_5/book_api/models.py
@@ -1,7 +1,12 @@
+"""It is database models module"""
+
 from sqlalchemy import Column, Integer, String
 from database import Base
 
 class Book(Base):
+
+    """This class is model of table named books"""
+
     __tablename__ = 'books'
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, nullable= False, index=True)
