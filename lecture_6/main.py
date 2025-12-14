11e53212b53dab9fab3b5b1e261af47666228a4c
Maksim Zaharenkov <m.zakharenkov@list.ru>
2025-12-14 22:41:04 +0300

    Add changes in main file

--- lecture_6/main.py
+++ lecture_6/main.py
@@ -1,7 +1,13 @@
+"""
+It is app for dockerize
+"""
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/healthcheck")
 async def healthcheck() -> dict:
+    """
+    This endpoint return status
+    """
     return {"status": "ok"}
