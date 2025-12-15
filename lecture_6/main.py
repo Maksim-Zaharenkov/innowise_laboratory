c7208f7f72089e66c6c628ea757f1615d8c36d40
Maksim Zaharenkov <m.zakharenkov@list.ru>
2025-12-16 00:54:04 +0300

    Add lecture_5 and dockerize it

--- lecture_6/main.py
+++ lecture_6/main.py
@@ -2,6 +2,7 @@
 It is app for dockerize
 """
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
@@ -11,3 +12,6 @@ async def healthcheck() -> dict:
     This endpoint return status
     """
     return {"status": "ok"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
